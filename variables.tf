2860eeb029cc03842f325cab022409ee336192dd
raphaelseguin <raphaelseguin@gmail.com>
2020-03-02 11:36:03 +0000

    deletions

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {}
-variable "resource_group_name" {}
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "machines_names" {}
-variable "machines_number" {}
-variable "public_key" {}
--- vars.auto.tfvars
+++
@@ -1,6 +0,0 @@
-location            = "westeurope"
-resource_group_name = "rg_pile_1"
-machines_number = 1
-machines_names = [
-    "jenkinsVM"
-]
